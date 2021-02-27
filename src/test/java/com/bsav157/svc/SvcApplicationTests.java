cab51330c870b2f9708f74f1203a154b8f87a385
ariasvargas15 <stevenson.arias@gmail.com>
2021-02-27 10:48:34 -0500

    Initial commit

---
+++ src/test/java/com/bsav157/svc/SvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsav157.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
